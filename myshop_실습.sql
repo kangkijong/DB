847d2572a5f9adf38421f18f82d3dc3e864c7102
kang <kijong4309@naver.com>
2025-07-23 18:19:15 +0900

    2025-07-23 commit

--- myshop_실습.sql
+++ myshop_실습.sql
@@ -2,100 +2,286 @@
 		실습 데이터베이스 연결 : myshop2019
 		실습 내용 - 기본적인 데이터 조회 	 
 ******************************************************/
+
+/* 반드시 기억해주세요!!! - 워크벤치 실행 시 마다 명령어 실행!!! */
+show databases;		-- 모든 데이터베이스 조회					-- hrdb2019, imformation_schema, market_db, myshop2019, mysql, performance_schema, sys
+use myshop2019;		-- 사용할 데이터베이스 오픈
+select database();		-- 데이터베이스 선택
+show tables;			-- 데이터베이스의 모든 테이블 조회	-- category, customer, employee, 
+																						-- order_detail, order_detail2016, order_detail2017, 
+																						-- order_header, order_header2016, order_header2017, 
+                                                                                        -- product, sub_category
+
 -- Q01) customer 테이블 모든 행과 열을 조회하고 어떤 열들이 있는지, 데이터 형식은 어떻게 되는지 살펴보세요.
+desc customer;			-- customer_id, customer_name, gender, phone, email, city, birth_date, register_date, point
+
 -- Q02) employee 테이블 모든 행과 열을 조회하고 어떤 열들이 있는지, 데이터 형식은 어떻게 되는지 살펴보세요.
+desc employee;		-- employee_id, employee_name, gender, phone, email, hire_date, retire_date
+
 -- Q03) product 테이블 모든 행과 열을 조회하고 어떤 열들이 있는지, 데이터 형식은 어떻게 되는지 살펴보세요.
+desc product;			-- product_id, product_name, sub_category_id
+
 -- Q04) order_header 테이블 모든 행과 열을 조회하고 어떤 열들이 있는지, 데이터 형식은 어떻게 되는지 살펴보세요.
+desc order_header;	-- order_id, customer_id, employee_id, order_date, sub_total, delivery_fee, total_due
+
 -- Q05) order_detail 테이블 모든 행과 열을 조회하고 어떤 열들이 있는지, 데이터 형식은 어떻게 되는지 살펴보세요.
+desc order_detail;		-- order_id, drder_detail_id, product_id, order_qty, unit_price, discount, line_total
+
 -- Q06) 모든 고객의 아이디, 이름, 지역, 성별, 이메일, 전화번호를 조회하세요.
+-- select *
+-- from customer;
+select customer_id as 아이디, customer_name as 고객명, city as 지역, gender as 성별, email as 이메일, phone as 전화번호
+from customer;
+
 -- Q07) 모든 직원의 이름, 사원번호, 성별, 입사일, 전화번호를 조회하세요.
+-- select *
+-- from employee;
+select employee_name as 직원명, employee_id as 사원번호, gender as 성별, hire_date as 입사일, phone as 전화번호
+from employee;
+
 -- Q08) 이름이 '홍길동'인 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where customer_name = '홍길동';
+
 -- Q09) 여자 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where gender = 'F';
+
 -- Q10) '울산' 지역 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where city = '울산';
+
 -- Q11) 포인트가 500,000 이상인 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where point >= 500000;
+
 -- Q12) 이름에 공백이 들어간 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where length(customer_name) != length(replace(customer_name, ' ', ''));
+
 -- Q13) 전화번호가 010으로 시작하지 않는 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where left(phone, 3) != '010';
+
 -- Q14) 포인트가 500,000 이상 '서울' 지역 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where point >= 500000 and city = '서울';
+
 -- Q15) 포인트가 500,000 이상인 '서울' 이외 지역 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where point >= 500000 and city != '서울';
+
 -- Q16) 포인트가 400,000 이상인 '서울' 지역 남자 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where point >= 400000 and city = '서울' and gender = 'M';
+
 -- Q17) '강릉' 또는 '원주' 지역 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where city = '강릉' or city = '원주';
+
 -- Q18) '서울' 또는 '부산' 또는 '제주' 또는 '인천' 지역 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where city = '서울' or city = '부산' or city = '제주' or city = '인천';
+
 -- Q19) 포인트가 400,000 이상, 500,000 이하인 고객의 이름, 아이디, 성별, 지역, 전화번호, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, point as 포인트
+from customer
+where point >= 400000 and point <= 500000;
+
 -- Q20) 1990년에 출생한 고객의 이름, 아이디, 성별, 지역, 전화번호, 생일, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, birth_date as 생일, point as 포인트
+from customer
+where left(birth_date, 4) = '1990';
+
 -- Q21) 1990년에 출생한 여자 고객의 이름, 아이디, 성별, 지역, 전화번호, 생일, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, birth_date as 생일, point as 포인트
+from customer
+where left(birth_date, 4) = '1990' and gender = 'F';
+
 -- Q22) 1990년에 출생한 '대구' 또는 '경주' 지역 남자 고객의 이름, 아이디, 성별, 지역, 전화번호, 생일, 포인트를 조회하세요.
+select customer_name as 고객명, customer_id as 아이디, gender as 성별, city as 지역, phone as 전화번호, birth_date as 생일, point as 포인트
+from customer
+where left(birth_date, 4) = '1990' and gender = 'M' and (city = '대구' or city = '경주');
+
 -- Q23) 1990년에 출생한 남자 고객의 이름, 아이디, 성별, 지역, 전화번호, 생일, 포인트를 조회하세요.
 --      단, 홍길동(gildong) 형태로 이름과 아이디를 묶어서 조회하세요.
+select concat(customer_name, '(', customer_id, ')') as '이름(아이디)', gender as 성별, city as 지역, phone as 전화번호, birth_date as 생일, point as 포인트
+from customer
+where left(birth_date, 4) = '1990' and gender = 'M';
+
 -- Q24) 근무중인 직원의 이름, 사원번호, 성별, 전화번호, 입사일를 조회하세요.
+select employee_name as 직원명, employee_id as 사원번호, gender as 성별, phone as 전화번호, hire_date as 입사일
+from employee;
+
 -- Q25) 근무중인 남자 직원의 이름, 사원번호, 성별, 전화번호, 입사일를 조회하세요.
+select employee_name as 직원명, employee_id as 사원번호, gender as 성별, phone as 전화번호, hire_date as 입사일
+from employee
+where gender = 'M';
+
 -- Q26) 퇴사한 직원의 이름, 사원번호, 성별, 전화번호, 입사일, 퇴사일를 조회하세요.
+select employee_name as 직원명, employee_id as 사원번호, gender as 성별, phone as 전화번호, hire_date as 입사일, retire_date as 퇴사일
+from employee
+where retire_date is not null;
 
 -- Q28) 2019-01-01 ~ 2019-01-07 기간 주문의 주문번호, 고객아이디, 사원번호, 주문일시, 소계, 배송비, 전체금액을 조회하세요.
 --      단, 고객아이디를 기준으로 오름차순 정렬해서 조회하세요.
+-- desc order_header;	-- order_id, customer_id, employee_id, order_date, sub_total, delivery_fee, total_due
+select order_id as 주문번호, customer_id as 고객아이디, employee_id as 사원번호, order_date as 주문일시, sub_total as 소계, delivery_fee as 배송비, total_due as 전체금액
+from order_header
+where order_date >= '2019-01-01' and order_date <= '2019-01-07';
     
 -- Q29) 2019-01-01 ~ 2019-01-07 기간 주문의 주문번호, 고객아이디, 사원번호, 주문일시, 소계, 배송비, 전체금액을 조회하세요.
 --      단, 전체금액을 기준으로 내림차순 정렬해서 조회하세요.
+select order_id as 주문번호, customer_id as 고객아이디, employee_id as 사원번호, order_date as 주문일시, sub_total as 소계, delivery_fee as 배송비, total_due as 전체금액
+from order_header
+where order_date >= '2019-01-01' and order_date <= '2019-01-07'
+order by total_due desc;
 
 -- Q30) 2019-01-01 ~ 2019-01-07 기간 주문의 주문번호, 고객아이디, 사원번호, 주문일시, 소계, 배송비, 전체금액을 조회하세요.
 --      단, 사원번호를 기준으로 오름차순, 같은 사원번호는 주문일시를 기준으로 내림차순 정렬해서 조회하세요.
+select order_id as 주문번호, customer_id as 고객아이디, employee_id as 사원번호, order_date as 주문일시, sub_total as 소계, delivery_fee as 배송비, total_due as 전체금액
+from order_header
+where order_date >= '2019-01-01' and order_date <= '2019-01-07'
+order by employee_id asc, order_date desc;
 
 /**
 	그룹함수
 **/
 /** customer 테이블 사용 **/
+-- customer_id, customer_name, gender, phone, email, city, birth_date, register_date, point
+
 -- Q01) 고객의 포인트 합을 조회하세요.
+select sum(point) as '모든 고객의 포인트 합'
+from customer;
 
 -- Q02) '서울' 지역 고객의 포인트 합을 조회하세요.
+select sum(point) as '서울 지역 고객 포인트 합'
+from customer
+where city = '서울';
 
 -- Q03) '서울' 지역 고객의 수를 조회하세요.
+select count(customer_id) as '서울 지역 고객 수'
+from customer
+where city = '서울';
 
 -- Q04) '서울' 지역 고객의 포인트 합과 평균을 조회하세요.
+select sum(point) as 합, avg(point) as 평균
+from customer
+where city = '서울';
      
 -- Q05) '서울' 지역 고객의 포인트 합, 평균, 최댓값, 최솟값을 조회하세요.
+select sum(point) as 합, avg(point) as 평균, max(point) as 최댓값, min(point) as 최솟값
+from customer
+where city = '서울';
 
 -- Q06) 남녀별 고객의 수를 조회하세요.
+select gender, count(*)
+from customer
+group by gender;
 
 -- Q07) 지역별 고객의 수를 조회하세요.
 --      단, 지역 이름을 기준으로 오름차순 정렬해서 조회하세요.
-
+select city, count(*)
+from customer
+group by city
+order by city asc;
  
 -- Q08) 지역별 고객의 수를 조회하세요.
 --      단, 고객의 수가 10명 이상인 행만 지역 이름을 기준으로 오름차순 정렬해서 조회하세요.
-   
+select city, count(*)
+from customer
+group by city
+having count(customer_id) >= 10
+order by city asc;
     
 -- Q09) 남녀별 포인트 합을 조회하세요.
+select gender, format(sum(point), 0) as '남녀별 포인트 합'
+from customer
+group by gender;
     
 -- Q10) 지역별 포인트 합을 조회하세요.
 --      단, 지역 이름을 기준으로 오름차순 정렬해서 조회하세요.
+select city, format(sum(point), 0) as '지역별 포인트 합'
+from customer
+group by city
+order by city asc;
     
 -- Q11) 지역별 포인트 합을 조회하세요.
 --      단, 포인트 합이 1,000,000 이상인 행만 포인트 합을 기준으로 내림차순 정렬해서 조회하세요.
-
+select city, format(sum(point), 0) as '지역별 포인트 합'
+from customer
+group by city
+having sum(point) >= 1000000
+order by city asc;
       
 -- Q12) 지역별 포인트 합을 조회하세요.
 --      단, 포인트 합을 기준으로 내림차순 정렬해서 조회하세요.
-   
+select city, format(sum(point), 0) as '지역별 포인트 합'
+from customer
+group by city
+order by sum(point) desc;
 
 -- Q13) 지역별 고객의 수, 포인트 합을 조회하세요.
 --      단, 지역 이름을 기준으로 오름차순 정렬해서 조회하세요.
-
+select city, count(*) as '고객 수', sum(point) as '포인트 합'
+from customer
+group by city
+order by city asc;
 
 -- Q14) 지역별 포인트 합, 포인트 평균을 조회하세요.
 --      단, 포인트가 NULL이 아닌 고객을 대상으로 하며, 지역 이름을 기준으로 오름차순 정렬해서 조회하세요.
+select city, sum(point) as '포인트 합', avg(point) as '포인트 평균'
+from customer
+where point is not null
+group by city
+order by city asc;
 
 -- Q15) '서울', '부산', '대구' 지역 고객의 지역별, 남녀별 포인트 합과 평균을 조회하세요.
 --      단, 지역 이름을 기준으로 오름차순, 같은 지역은 성별을 기준으로 오름차순 정렬해서 조회하세요.
-
+select city, gender, sum(point) as '포인트 합', avg(point) as '포인트 평균'
+from customer
+where city in ('서울', '부산', '대구')
+group by city, gender
+order by city asc, gender asc;
 
 /** order_header 테이블 사용 **/
-    
--- Q16) 2019년 1월 주문에 대하여 고객아이디별 전체금액 합을 조회하세요.
+-- order_id, customer_id, employee_id, order_date, sub_total, delivery_fee, total_due
 
+-- Q16) 2019년 1월 주문에 대하여 고객아이디별 전체금액 합을 조회하세요.
+select customer_id as 고객아이디, format(total_due, 0) as 전체금액, order_date as 주문일
+from order_header
+where left(order_date, 7) = '2019-01';
 
 -- Q17) 주문연도별 전체금액 합계를 조회하세요.
+select left(order_date, 4) as 주문연도, format(sum(total_due), 0) as 전체금액
+from order_header
+group by left(order_date, 4);
 
 -- Q18) 2019.01 ~ 2019.06 기간 주문에 대하여 주문연도별, 주문월별 전체금액 합을 조회하세요.
+select left(order_date, 7) as '주문연도-주문월', format(sum(total_due), 0) as '전체금액 합'
+from order_header
+-- where order_date >= '2019-01-01' and order_date <= '2019-06-30'
+where order_date between cast('2019-01-01' as date) and cast('2019-06-30' as date)
+group by left(order_date, 7);
 
 -- Q19) 2019.01 ~ 2019.06 기간 주문에 대하여 주문연도별, 주문월별 전체금액 합과 평균을 조회하세요.
+select left(order_date, 7) as '주문연도-주문월', format(sum(total_due), 0) as '전체금액 합', format(avg(total_due), 0) as '전체금액 평균'
+from order_header
+where order_date between cast('2019-01-01' as date) and cast('2019-06-30' as date)
+group by left(order_date, 7);
 
 -- Q20) 주문연도별, 주문월별 전체금액 합과 평균을 조회하고, rollup 함수를 이용하여 소계와 총계를 출력해주세요.
+select left(order_date, 7) as '주문연도-주문월', format(sum(total_due), 0) as '전체금액 합', format(avg(total_due), 0) as '전체금액 평균'
+from order_header
+group by left(order_date, 7) with rollup;
